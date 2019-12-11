9e535d10f46b4bbc4bf049e394f08ad9c07cde93
Anton Stepanov <anton.stepanov@dailybonus.io>
2019-12-11 15:56:16 +0100

    product profile example

---
+++ data/product-profile.js
@@ -0,0 +1,103 @@
+
+
+const example1 = {
+
+    product: [
+        {
+            type: 'about',
+            items: [
+                {
+                    photo: '',
+                    map: '',
+                    title: '',
+                    description: ''
+                }
+            ]
+        },
+        {
+            type: 'company',
+            items: [
+                {
+                    photo: '',
+                    map: '',
+                    title: '',
+                    description: ''
+                }
+            ]
+        },
+
+    ],
+
+    manufacturing: [
+        {
+            type: 'location',
+            items: [
+                {
+                    photo: '',
+                    map: '',
+                    title: '',
+                    description: ''
+                }
+            ]
+        },
+        {
+            type: 'components',
+            items: [
+                {
+                    photo: ''
+                    title: '',
+                    description: ''
+                },
+                {
+                    photo: ''
+                    title: '',
+                    description: ''
+                }
+            ]
+        },
+        {
+            type: 'labor',
+            items: [
+                {
+                    photo: ''
+                    title: '',
+                    description: ''
+                }
+            ]
+        },
+    ],
+    
+
+    recycle: [
+        {
+            type: 'collection',
+        },
+        {
+            type: 'sorting',
+        },
+        {
+            type: 'processing',
+        },
+        {
+            type: 'manufacturing',
+        },
+    ],
+
+    waste_impact: [
+        {
+            type: 'decomposition',
+            title: 'pastic 20,000 '
+            description: ''
+        },
+        {
+            type: 'waste_score',
+            title: 'pastic 20,000 '
+            description: ''
+        },
+
+    ]
+
+
+
+
+}
